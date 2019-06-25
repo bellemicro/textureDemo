bbf0a0a582d64b470103868f3ea29ac424c25eb2
Zhang_yan <bellemicro@outlook.com>
2019-06-25 16:21:50 +0800

    Initial Commit

---
+++ testTexttureDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTexttureDemo
+//
+//  Created by mobstazinc_ios_developer on 2019/6/25.
+//  Copyright © 2019年 mobstazinc_ios_developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
